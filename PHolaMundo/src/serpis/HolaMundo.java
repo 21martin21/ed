8b73a04668cef5131474815ca2ab49a885d9fe66
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2012-11-26 11:50:14 +0100

    Añadiendo PHolaMundo a git.

---
+++ PHolaMundo/src/serpis/HolaMundo.java
@@ -0,0 +1,15 @@
+package serpis;
+
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola desde java con eclipse (y 2).");
+
+	}
+
+}
